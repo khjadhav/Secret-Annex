fe54d5b43487ed28fc9e63f25b029d00f52f1d85
khjadhav <32267919+khjadhav@users.noreply.github.com>
2017-11-16 22:28:06 -0500

    anime animation+hover bg color

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 
+// $(document).ready(function() {
+// $("#cube").click(function() {
+//     $("#grey").animate({"margin-top": "50px"})(5000);
+//     $("#lightGrey").animate({"margin-top": "50px"});
+//     // $("#grey").css({"margin-top":"50px"});
+//     // $("#lightGrey").css({"margin-top":"50px"});
+// })
+// })
+
 $(document).ready(function() {
-$("#cube").click(function() {
-    $("#grey").animate({"margin-top": "50px"})(5000);
-    $("#lightGrey").animate({"margin-top": "50px"});
-    // $("#grey").css({"margin-top":"50px"});
-    // $("#lightGrey").css({"margin-top":"50px"});
-})
+    $("#cube").click(function() {
+        anime({
+            targets:'div.grey',
+            translateY: [
+            { value: 150, duration: 800, easing: 'easeInOutSine'},
+            ]
+            })
+        anime({
+            targets:'div.lightGrey',
+            translateY: [
+            { value: 300, duration: 800, easing: 'easeInOutSine'},
+            ]
+            })
+    })
 })
