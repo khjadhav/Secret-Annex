d925ccb620500a5b68ea9c9aed5b140c9a398551
khjadhav <32267919+khjadhav@users.noreply.github.com>
2017-11-16 21:30:12 -0500

    translating the objects on click

---
+++ main.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function() {
+$("#cube").click(function() {
+    $("#grey").animate({"margin-top": "50px"})(5000);
+    $("#lightGrey").animate({"margin-top": "50px"});
+    // $("#grey").css({"margin-top":"50px"});
+    // $("#lightGrey").css({"margin-top":"50px"});
+})
+})
