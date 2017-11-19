22fb626bf95f37ab7d4e6e60c26401d25e2138fa
khjadhav <32267919+khjadhav@users.noreply.github.com>
2017-11-19 18:47:06 -0500

    modal displaying
    display modal

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
 
-// $(document).ready(function() {
-// $("#cube").click(function() {
-//     $("#grey").animate({"margin-top": "50px"})(5000);
-//     $("#lightGrey").animate({"margin-top": "50px"});
-//     // $("#grey").css({"margin-top":"50px"});
-//     // $("#lightGrey").css({"margin-top":"50px"});
-// })
-// })
-
 $(document).ready(function() {
+    // { value: 0, duration: 1000, delay:10, easing: 'easeOutExpo' },
     $("#cube").click(function() {
+
         anime({
             targets:'div.grey',
             translateY: [
@@ -22,10 +15,44 @@ $(document).ready(function() {
             { value: 200, duration: 800, easing: 'easeInOutSine'},
             ]
             })
+
+        anime({
+                targets: '#lineStroke',
+                scaleY:[
+                { value: 30, duration: 2000, delay:10, easing: 'easeOutExpo' },
+                ]
+            })
+        $(this).parents('.all').addClass('expanded');
     })
-        // anime({
-        //     targets:'div.all',
-        //     scale: '*=2',
-        //     })
+
+    $(document).on("mouseenter", ".expanded .grey img", function() {
+      $(this).parents('.all').removeClass('grey-active black-active light-grey-active');
+      $(this).parents('.all').addClass('grey-active');
+    }).on('mouseleave', function() {
+      $(this).parents('.all').removeClass('grey-active');
+    });
+
+    $(document).on("mouseenter", ".expanded .black img", function() {
+      $(this).parents('.all').removeClass('grey-active black-active light-grey-active');
+      $(this).parents('.all').addClass('black-active');
+    }).on('mouseleave', function() {
+      $(this).parents('.all').removeClass('black-active');
+    });
+
+    $(document).on("mouseenter", ".expanded .lightGrey img", function() {
+      $(this).parents('.all').removeClass('grey-active black-active light-grey-active');
+      $(this).parents('.all').addClass('light-grey-active');
+    }).on('mouseleave', function() {
+      $(this).parents('.all').removeClass('light-grey-active');
+    });
+
+// // When the user clicks the button, open the modal
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
+
+        $(".black").click(function(){
+        $("#myModal").css({display:"block"});
+        })
 
 })
