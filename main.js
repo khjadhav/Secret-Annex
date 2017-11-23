fb185a4c3e79cf7411e17afcca72e8036f49e84a
khjadhav <32267919+khjadhav@users.noreply.github.com>
2017-11-23 12:54:54 -0500

    zoom not working

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 
 $(document).ready(function() {
-// $(".black").zoomTarget();
-    // { value: 0, duration: 1000, delay:10, easing: 'easeOutExpo' },
     $(".black").click(function() {
+        $(".black").click(function(evt) {
+        $(this).zoomTo({targetsize:1.5, duration:1600, closeclick:false,});
+        evt.stopPropagation();
+        });
+    $(".all").click(function() {
+        console.log("print");
+    });
 
         anime({
             targets:'div.grey',
@@ -26,25 +31,22 @@ $(document).ready(function() {
         $(this).parents('.all').children().addClass('expanded');
         //check if expanded is getting added to all the classes
         console.log($(this).parents('.all').children());
-    })
+        $(".grey-cube").bind("mouseenter", function() {
+            if ($(this).parent().hasClass('notActive')){
+                $(this).parent().removeClass("notActive");
+            }
+            console.log(this);
+            if ($(this).hasClass("notActive") ){
+                $(this).removeClass("notActive");
+                $(this).siblings().removeClass("active");
+            }
 
-//for selecting individual floors on the grey floor
-    $(".grey-cube").bind("mouseenter", function() {
-        if ($(this).parent().hasClass('notActive')){
-            $(this).parent().removeClass("notActive");
-        }
-        console.log(this);
-        if ($(this).hasClass("notActive") ){
-            $(this).removeClass("notActive");
-            $(this).siblings().removeClass("active");
-        }
+            $(this).addClass("active");
+            $(this).siblings().addClass('notActive');
+            $(this).parent().siblings().addClass("notActive");
+        });
 
-        $(this).addClass("active");
-        $(this).siblings().addClass('notActive');
-        $(this).parent().siblings().addClass("notActive");
-    });
-
-    $(".black-cube").bind("mouseenter", function() {
+            $(".black-cube").bind("mouseenter", function() {
 
         if ($(this).parent().hasClass('notActive')){
             $(this).parent().removeClass("notActive");
@@ -74,37 +76,49 @@ $(document).ready(function() {
         $(this).siblings().addClass('notActive');
         $(this).parent().siblings().addClass("notActive");
     });
+    })
+
+//for selecting individual floors on the grey floor
 
 
 
 
-//when the user clicks on an item open the modal
-        $("#cube").click(function(){
-            $("#cube1modal").css({display:"block"});
-        })
 
-        $("#cube1").click(function(){
-            $("#cube2modal").css({display:"block"});
-        })
 
-        $("#cube2").click(function(){
-            $("#cube3modal").css({display:"block"});
-        })
 
-        $(".grey").click(function(){
-        $("#grey").css({display:"block"});
-        //console.log("grey");
-        })
+//MODALS
+//         $("#cube").click(function(){
+//             var pos=$(this).position();
+//             console.log(pos);
+//             $("#cube1modal").css({display:"block"})
+//             $("#cube1modal").children().css({left: pos.left+"px"});
+//         })
 
-        $(".lightGrey").click(function(){
-        $("#lightGrey").css({display:"block"});
-        //console.log("lightgrey");
-        })
+//         $("#cube1").click(function(){
+//             var pos=$(this).position();
+//             $("#cube2modal").css({display:"block"});
+//             $("#cube2modal").children().css({left: pos.left+"px"});
+//         })
 
-// when the user clicks on on close, it goes away
-        $(".close").click(function(){
-            $(this).parents(".modal").css({display:"none"});
-            //console.log("close");
-        })
+//         $("#cube2").click(function(){
+//             var pos=$(this).position();
+//             $("#cube3modal").css({display:"block"});
+//             $("#cube3modal").children().css({left:pos.left+"px"});
+//         })
 
+//         $(".grey").click(function(){
+//         $("#grey").css({display:"block"});
+//         //console.log("grey");
+//         })
+
+//         $(".lightGrey").click(function(){
+//         $("#lightGrey").css({display:"block"});
+//         //console.log("lightgrey");
+//         })
+
+// // when the user clicks on on close, it goes away
+//         $(".close").click(function(){
+//             $(this).parents(".modal").css({display:"none"});
+//             //console.log("close");
+//         })
 })
