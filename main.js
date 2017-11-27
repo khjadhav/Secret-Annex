b16d1f6a72dcdd60b1ea9f860c21b37f1ee94bf0
khjadhav <32267919+khjadhav@users.noreply.github.com>
2017-11-27 12:40:12 -0500

    fade ins added to the rooms

--- main.js
+++ main.js
@@ -8,18 +8,30 @@ $(document).ready(function() {
         anime({
             targets:'div.grey',
             translateY: [
-            { value: 140, duration: 800 , easing: 'easeInOutSine'},
-            ]
+            { value: 210, duration: 800 , easing: 'easeInOutSine'},
+            ],
+            zIndex:{
+              value:[500,550]
+            }
             })
+
         anime({
             targets:'div.lightGrey',
             translateY: [
-            { value: 360, duration: 800, easing: 'easeInOutSine'},
-            ]
+            { value: 440, duration: 800, easing: 'easeInOutSine'},
+            ],
+            zIndex:{
+              value:[250,200]
+            }
+
             })
+
 
         $(this).parents('.all').children().addClass('expanded');
 
+         var height_responsive=$(".all-wrapper").height();
+      $(".all-wrapper").css({height:"1200px"});
+      $(".all").css({height:"1200px"});
       $(document).on( "mouseenter", ".expanded .grey-cube", function() {
       if ($(this).parent().hasClass('notActive')){
           $(this).parent().removeClass("notActive");
@@ -43,6 +55,7 @@ $(document).ready(function() {
         $("#bathroom").css({opacity:"1",right:pos.right, width:"100%"});
         $("#entrance").css({opacity:"1",right:pos.right, width:"100%"});
       }
+
     });
 
     //for selecting individual floors on the black floor
@@ -79,7 +92,7 @@ $(document).ready(function() {
 
       if ($(this).siblings().hasClass("modal")){
         var pos=$(this).position()
-        $("#vanpels").css({opacity:"1",right:pos.right, width:"100%"});
+        $("#vanpels").css({opacity:"1", width:"100%"});
         $("#peter").css({opacity:"1",right:pos.right, width:"100%"});
         $("#anne").css({opacity:"1",right:pos.right, width:"100%"});
         $("#franks").css({opacity:"1",right:pos.right, width:"100%"});
@@ -88,6 +101,8 @@ $(document).ready(function() {
       }
 
     });
+
+
   })
 
 // $("#cube").elevateZoom({
@@ -146,6 +161,11 @@ $(document).ready(function() {
 
 
         $("#cube6").click(function(){
+            // var container = $("<div></div>",{class:'checkout-product-div'});
+            // container.appendTo($(".checkout-cart"));
+
+          var overlay1 = $("<div></div>",{id:'overlay'});
+          overlay1.appendTo($('body'));
           $("#bathroom").css({display:"block"});
         });
 
@@ -164,6 +184,14 @@ $(document).ready(function() {
 
         $("#cube3").click(function(){
           $("#vanpels").css({display:"block"});
+          anime({
+            targets: 'div.modal',
+            // top: '80%', // Animate all divs left position to 80%
+            opacity: 1, // Animate all divs opacity to .8
+            duration: 800 ,
+            easing: 'easeInOutSine',
+            backgroundColor: '#FFF' // Animate all divs background color to #FFF
+            });
         })
 
 
@@ -177,7 +205,10 @@ $(document).ready(function() {
 
         $(".close").click(function(){
             $(this).parents(".modal").css({display:"none"});
+            overlay.remove();
             //console.log("close");
         })
+
+
 
 })
