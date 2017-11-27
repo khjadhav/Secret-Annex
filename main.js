c7cb288bcd38cdc676e3f3528be606a4511baad1
khjadhav <32267919+khjadhav@users.noreply.github.com>
2017-11-26 20:07:42 -0500

    switched back to old zoom

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 
 $(document).ready(function() {
-
-  /* Zoom animation end Call back */
-  function endCb() {
-    // can be used to trigger any event once the zoom animation is triggered
-    console.log('test123');
-
-      // $(window).scrollTop($('.grey-cube').offset().top);
-  }
     $(".black").click(function(e) {
 
         anime({
@@ -84,47 +76,6 @@ $(document).ready(function() {
       $(this).parent().siblings().addClass("notActive");
     });
   })
-
-  // $(".grey-cube").click(function(e) {
-  //   //e.stopPropagation();
-  // });
-  // var zoomVar = 0;
-  // $('body').click(function(e) {
-
-  //     e.stopPropagation();
-  //     if($(e.target).hasClass("grey-cube"))
-  //     {
-  //       $(e.target).zoomTo({targetsize:1,duration:1600,closeclick:true,animationendcallback:endCb});
-  //       if(zoomVar == 0 )
-  //       {
-  //         zoomVar = 1;
-  //       }
-  //       else {
-  //         zoomVar = 0;
-  //       }
-  //     }
-  //     if( zoomVar == 0)
-  //     {
-  //       $("body").zoomTo({targetsize:0,duration:2600});
-  //     }
-
-  //     console.log('test-close');
-  //     console.log(e);
-  //     $("body").zoomTo({targetsize:0,duration:2600});
-  // });
-
-    //for selecting individual floors on the grey floor
-
-
-
-
-//when the user clicks on an item open the modal
-        // $("#cube3").mouseenter(function(){
-        //   var pos=$(this).position();
-        //   var width=$(document).width();
-        //   console.log(pos);
-        //   $("#cube1modal").css({display:"block"});
-        // })
 
 
         var cube3enter= function(){
