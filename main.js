78dc889eee15aa558fbe0b613b8f8a2977812498
khjadhav <32267919+khjadhav@users.noreply.github.com>
2017-11-27 23:01:05 -0500

    positioning modals

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+var overlay1 = $("<div id='overlay'></div>");
 $(document).ready(function() {
     $(".black").click(function(e) {
 
@@ -8,7 +8,7 @@ $(document).ready(function() {
         anime({
             targets:'div.grey',
             translateY: [
-            { value: 210, duration: 800 , easing: 'easeInOutSine'},
+            { value: 280, duration: 800 , easing: 'easeInOutSine'},
             ],
             zIndex:{
               value:[500,550]
@@ -18,7 +18,7 @@ $(document).ready(function() {
         anime({
             targets:'div.lightGrey',
             translateY: [
-            { value: 440, duration: 800, easing: 'easeInOutSine'},
+            { value: 580, duration: 800, easing: 'easeInOutSine'},
             ],
             zIndex:{
               value:[250,200]
@@ -27,9 +27,9 @@ $(document).ready(function() {
             })
 
 
-        $(this).parents('.all').children().addClass('expanded');
+      $(this).parents('.all').children().addClass('expanded');
 
-         var height_responsive=$(".all-wrapper").height();
+      var height_responsive=$(".all-wrapper").height();
       $(".all-wrapper").css({height:"1200px"});
       $(".all").css({height:"1200px"});
       $(document).on( "mouseenter", ".expanded .grey-cube", function() {
@@ -48,10 +48,10 @@ $(document).ready(function() {
 
       if ($(this).siblings().hasClass("modal")){
         var pos=$(this).position()
-        $("#vanpels").css({opacity:"1",right:pos.right, width:"100%"});
-        $("#peter").css({opacity:"1",right:pos.right, width:"100%"});
-        $("#anne").css({opacity:"1",right:pos.right, width:"100%"});
-        $("#franks").css({opacity:"1",right:pos.right, width:"100%"});
+        $("#vanpels").css({opacity:"1", position:"absolute", top:"-174px", left:"375px", width:"100%"});
+        $("#peter").css({opacity:"1",position:"absolute", top:"-136px", left:"-405px", width:"100%"});
+        $("#anne").css({opacity:"1",position:"absolute", top:"-387px", left:"378px", width:"100%"});
+        $("#franks").css({opacity:"1",position:"absolute", top:"-490px", width:"100%"});
         $("#bathroom").css({opacity:"1",right:pos.right, width:"100%"});
         $("#entrance").css({opacity:"1",right:pos.right, width:"100%"});
       }
@@ -92,10 +92,10 @@ $(document).ready(function() {
 
       if ($(this).siblings().hasClass("modal")){
         var pos=$(this).position()
-        $("#vanpels").css({opacity:"1", width:"100%"});
-        $("#peter").css({opacity:"1",right:pos.right, width:"100%"});
-        $("#anne").css({opacity:"1",right:pos.right, width:"100%"});
-        $("#franks").css({opacity:"1",right:pos.right, width:"100%"});
+        $("#vanpels").css({opacity:"1", position:"absolute", top:"0px", left:"0px", right:pos.right, width:"100%"});
+        $("#peter").css({opacity:"1",position:"absolute", top:"-136px", left:"-405px", width:"100%"});
+        $("#anne").css({opacity:"1",position:"absolute", top:"-387px", left:"378px", width:"100%"});
+        $("#franks").css({opacity:"1",position:"absolute", top:"-490px", width:"100%"});
         $("#bathroom").css({opacity:"1",right:pos.right, width:"100%"});
         $("#entrance").css({opacity:"1",right:pos.right, width:"100%"});
       }
@@ -105,93 +105,127 @@ $(document).ready(function() {
 
   })
 
+        $("#enter").click(function(e) {
+        anime({
+            targets:'div.bookshelf',
+            translateX: [
+            { value: -20, duration: 800 , easing: 'easeInOutSine'},
+            ],
+            translateY: [
+            { value: -2090, duration: 800 , easing: 'easeInOutSine'},
+            ],
+
+            zIndex:{
+              value:[500,550]
+            }
+            })
+      });
+
 // $("#cube").elevateZoom({
 //   zoomType : "lens",
 //   lensShape : "round",
 //   lensSize    : 200,
 // });
-//for hovering over the VanPel's room
-        // var cube3enter= function(){
-        //   $("#vanpels").css({display:"block"});
-        //   };
-
-        // var cube3exit= function(){
-        //   $("#vanpels").css({display:"none"});
-        //   };
-
-        // $("#cube3").hover(cube3enter, cube3exit);
-        // $("#vanpels").hover(cube3enter, cube3exit);
-
-
-//for hovering over Peter's room
-//         var cube4enter= function(){
-//           $("#peter").css({display:"block"});
-//           };
-
-//         var cube4exit= function(){
-//           $("#peter").css({display:"none"});
-//           };
-
-//         $("#cube4").hover(cube4enter, cube4exit);
-//         $("#peter").hover(cube4enter, cube4exit);
-
-// //for hovering over Anne's room
-//         var cube7enter= function(){
-//           $("#anne").css({display:"block"});
-//           };
-
-//         var cube7exit= function(){
-//           $("#anne").css({display:"none"});
-//           };
-
-//         $("#cube7").hover(cube7enter, cube7exit);
-//         $("#anne").hover(cube7enter, cube7exit);
-
-// //for hovering over Franks's room
-//         var cube8enter= function(){
-//           $("#anne").css({display:"block"});
-//           };
-
-//         var cube8exit= function(){
-//           $("#anne").css({display:"none"});
-//           };
-
-//         $("#cube8").hover(cube8enter, cube8exit);
-//         $("#franks").hover(cube8enter, cube8exit);
-
-
-        $("#cube6").click(function(){
-            // var container = $("<div></div>",{class:'checkout-product-div'});
-            // container.appendTo($(".checkout-cart"));
-
-          var overlay1 = $("<div></div>",{id:'overlay'});
-          overlay1.appendTo($('body'));
-          $("#bathroom").css({display:"block"});
-        });
-
-        $("#cube5").click(function(){
-          $("#entrance").css({display:"block"});
-        });
-
-
-        $("#cube7").click(function(){
-          $("#anne").css({display:"block"});
-        });
-
-        $("#cube8").click(function(){
-          $("#franks").css({display:"block"});
-        })
-
-        $("#cube3").click(function(){
+// for hovering over the VanPel's room
+        var cube3enter= function(){
           $("#vanpels").css({display:"block"});
-          anime({
-            targets: 'div.modal',
-            // top: '80%', // Animate all divs left position to 80%
-            opacity: 1, // Animate all divs opacity to .8
-            duration: 800 ,
-            easing: 'easeInOutSine',
-            backgroundColor: '#FFF' // Animate all divs background color to #FFF
-            });
+          };
+
+        var cube3exit= function(){
+          $("#vanpels").css({display:"none"});
+          };
+
+        $("#cube3").hover(cube3enter, cube3exit);
+        $("#vanpels").hover(cube3enter, cube3exit);
+
+
+// for hovering over Peter's room
+        var cube4enter= function(){
+          $("#peter").css({display:"block"});
+          };
+
+        var cube4exit= function(){
+          $("#peter").css({display:"none"});
+          };
+
+        $("#cube4").hover(cube4enter, cube4exit);
+        $("#peter").hover(cube4enter, cube4exit);
+
+//for hovering over Anne's room
+        var cube7enter= function(){
+          $("#anne").css({display:"block"});
+          };
+
+        var cube7exit= function(){
+          $("#anne").css({display:"none"});
+          };
+
+        $("#cube7").hover(cube7enter, cube7exit);
+        $("#anne").hover(cube7enter, cube7exit);
+
+//for hovering over Franks's room
+        var cube8enter= function(){
+          $("#franks").css({display:"block"});
+          };
+
+        var cube8exit= function(){
+          $("#franks").css({display:"none"});
+          };
+
+        $("#cube8").hover(cube8enter, cube8exit);
+        $("#franks").hover(cube8enter, cube8exit);
+
+//for hovering over the bathroom
+        var cube6enter= function(){
+          $("#bathroom").css({display:"block"});
+          };
+
+        var cube6exit= function(){
+          $("#bathroom").css({display:"none"});
+          };
+
+        $("#cube6").hover(cube6enter, cube6exit);
+        $("#bathroom").hover(cube6enter, cube6exit);
+
+//for hovering over the entrance
+        var cube5enter= function(){
+          $("#entrance").css({display:"block"});
+          };
+
+        var cube5exit= function(){
+          $("#entrance").css({display:"none"});
+          };
+
+        $("#cube5").hover(cube5enter, cube5exit);
+        $("#entrance").hover(cube5enter, cube5exit);
+
+
+
+        // $("#cube6").click(function(){
+        //     // var container = $("<div></div>",{class:'checkout-product-div'});
+        //     // container.appendTo($(".checkout-cart"));
+
+        //   var overlay1 = $("<div></div>",{id:'overlay'});
+        //   // overlay1.appendTo($('body'));
+        //   $('body').append(overlay1);
+        //   $("#bathroom").css({display:"block"});
+        // });
+
+        // $("#cube5").click(function(){
+        //   $("#entrance").css({display:"block"});
+        // });
+
+
+        // $("#cube7").click(function(){
+        //   $("#anne").css({display:"block"});
+        // });
+
+        // $("#cube8").click(function(){
+        //   $("#franks").css({display:"block"});
+        // })
+
+        // $("#cube3").click(function(){
+        //   $("#vanpels").css({display:"block"});
         })
 
 
@@ -211,4 +245,4 @@ $(document).ready(function() {
 
 
 
-})
+
