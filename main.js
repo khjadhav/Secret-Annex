7890d78d58db571e65f71c79fe8cb50d5c105a69
khjadhav <32267919+khjadhav@users.noreply.github.com>
2017-11-29 08:50:29 -0500

    roof instruction

--- main.js
+++ main.js
@@ -1,60 +1,68 @@
 
 $(document).ready(function() {
+// Go into "exploded view" when the roof is clicked. Move the 2 floors down.
     $(".black").click(function(e) {
-
+      //move level one down
         anime({
             targets:'div.grey',
             translateY: [
             { value: 280, duration: 800 , easing: 'easeInOutSine'},
             ],
-            zIndex:{
-              value:[500,550]
-            }
             })
-
+      //move level 0 down
         anime({
             targets:'div.lightGrey',
             translateY: [
             { value: 580, duration: 800, easing: 'easeInOutSine'},
             ],
-            zIndex:{
-              value:[250,200]
-            }
-
+            })
+         anime({
+            targets:'div.thankyou',
+            translateY: [
+            { value: 680, duration: 800, easing: 'easeInOutSine'},
+            ],
             })
 
-
+      $("#roofinstruction").css({opacity:"0"});
+        // add the expanded class to all the divs under the all class
       $(this).parents('.all').children().addClass('expanded');
+        // add a height to the viewport
+      $(".all-wrapper").css({height:"1300px"});
+      $(".all").css({height:"1300px"});
 
-      $(".all-wrapper").css({height:"1200px"});
-      $(".all").css({height:"1200px"});
+      // When you hover over any room on level 1 ( with class grey), add the active class to that room and add inactive to everything else
       $(document).on( "mouseenter", ".expanded .grey-cube", function() {
-      if ($(this).parent().hasClass('notActive')){
-          $(this).parent().removeClass("notActive");
-      }
-      console.log(this);
-      if ($(this).hasClass("notActive") ){
-          $(this).removeClass("notActive");
-          $(this).siblings().removeClass("active");
-      }
 
-      $(this).addClass("active");
-      $(this).siblings().addClass('notActive');
-      $(this).parent().siblings().addClass("notActive");
+        // If the parent of the cube has non active class, remove the non active class
+        if ($(this).parent().hasClass('notActive')){
+            $(this).parent().removeClass("notActive");
+        }
+        console.log(this);
+        // If the cube has non active class, remove the non active class and remove active class from it's siblings
+        if ($(this).hasClass("notActive") ){
+            $(this).removeClass("notActive");
+            $(this).siblings().removeClass("active");
+        }
+        // add active class to the cube
+        $(this).addClass("active");
+        // add non-active class to the siblings
+        $(this).siblings().addClass('notActive');
+        // add non-active class to the parent of siblings
+        $(this).parent().siblings().addClass("notActive");
 
-      if ($(this).siblings().hasClass("modal")){
-        var pos=$(this).position()
-        $("#vanpels").css({opacity:"1", width:"100%"});
-        $("#peter").css({opacity:"1", width:"100%"});
-        $("#anne").css({opacity:"1", width:"100%"});
-        $("#franks").css({opacity:"1", width:"100%"});
-        $("#bathroom").css({opacity:"1", width:"100%"});
-        $("#entrance").css({opacity:"1", width:"100%"});
-      }
-
+        //make sure all the hovered on modals are 100% opacity
+        if ($(this).siblings().hasClass("modal")){
+          var pos=$(this).position()
+          $("#vanpels").css({opacity:"1", width:"100%"});
+          $("#peter").css({opacity:"1", width:"100%"});
+          $("#anne").css({opacity:"1", width:"100%"});
+          $("#franks").css({opacity:"1", width:"100%"});
+          $("#bathroom").css({opacity:"1", width:"100%"});
+          $("#entrance").css({opacity:"1", width:"100%"});
+        }
     });
 
-    //for selecting individual floors on the black floor
+    // When you hover over any room on the roof level ( with class black), add the active class to that room and add inactive to everything else
     $(document).on( "mouseenter", ".expanded .black-cube", function() {
       if ($(this).parent().hasClass('notActive')){
           $(this).parent().removeClass("notActive");
@@ -71,7 +79,7 @@ $(document).ready(function() {
 
     });
 
-    //for selecting individual floors on the light grey floor
+    // When you hover over any room on level 0 ( with class light-grey), add the active class to that room and add inactive to everything else
     $(document).on( "mouseenter", ".expanded .light-grey-cube", function() {
       if ($(this).parent().hasClass('notActive')){
           $(this).parent().removeClass("notActive");
@@ -95,34 +103,10 @@ $(document).ready(function() {
         $("#bathroom").css({opacity:"1", width:"100%"});
         $("#entrance").css({opacity:"1", width:"100%"});
       }
-
     });
-
-
   })
 
-        $("#enter").click(function(e) {
-        anime({
-            targets:'div.bookshelf',
-            translateX: [
-            { value: -20, duration: 800 , easing: 'easeInOutSine'},
-            ],
-            translateY: [
-            { value: -2090, duration: 800 , easing: 'easeInOutSine'},
-            ],
-
-            zIndex:{
-              value:[500,550]
-            }
-            })
-      });
-
-// $("#cube").elevateZoom({
-//   zoomType : "lens",
-//   lensShape : "round",
-//   lensSize    : 200,
-// });
-// for hovering over the VanPel's room
+      // for displaying the modal when you hover over VanPel's room
         var cube3enter= function(){
           $("#vanpels").css({display:"block"});
           };
@@ -135,7 +119,7 @@ $(document).ready(function() {
         $("#vanpels").hover(cube3enter, cube3exit);
 
 
-// for hovering over Peter's room
+      // for displaying the modal when you hover over Peters's room
         var cube4enter= function(){
           $("#peter").css({display:"block"});
           };
@@ -147,7 +131,7 @@ $(document).ready(function() {
         $("#cube4").hover(cube4enter, cube4exit);
         $("#peter").hover(cube4enter, cube4exit);
 
-//for hovering over Anne's room
+      // for displaying the modal when you hover over Anne's room
         var cube7enter= function(){
           $("#anne").css({display:"block"});
           };
@@ -159,7 +143,7 @@ $(document).ready(function() {
         $("#cube7").hover(cube7enter, cube7exit);
         $("#anne").hover(cube7enter, cube7exit);
 
-//for hovering over Franks's room
+      // for displaying the modal when you hover over Frank's room
         var cube8enter= function(){
           $("#franks").css({display:"block"});
           };
@@ -171,7 +155,7 @@ $(document).ready(function() {
         $("#cube8").hover(cube8enter, cube8exit);
         $("#franks").hover(cube8enter, cube8exit);
 
-//for hovering over the bathroom
+      // for displaying the modal when you hover over the bathroom
         var cube6enter= function(){
           $("#bathroom").css({display:"block"});
           };
@@ -183,7 +167,7 @@ $(document).ready(function() {
         $("#cube6").hover(cube6enter, cube6exit);
         $("#bathroom").hover(cube6enter, cube6exit);
 
-//for hovering over the entrance
+      // for displaying the modal when you hover over the entrance
         var cube5enter= function(){
           $("#entrance").css({display:"block"});
           };
@@ -194,49 +178,12 @@ $(document).ready(function() {
 
         $("#cube5").hover(cube5enter, cube5exit);
         $("#entrance").hover(cube5enter, cube5exit);
-
-
-
-        // $("#cube6").click(function(){
-        //     // var container = $("<div></div>",{class:'checkout-product-div'});
-        //     // container.appendTo($(".checkout-cart"));
-
-        //   var overlay1 = $("<div></div>",{id:'overlay'});
-        //   // overlay1.appendTo($('body'));
-        //   $('body').append(overlay1);
-        //   $("#bathroom").css({display:"block"});
-        // });
-
-        // $("#cube5").click(function(){
-        //   $("#entrance").css({display:"block"});
-        // });
-
-
-        // $("#cube7").click(function(){
-        //   $("#anne").css({display:"block"});
-        // });
-
-        // $("#cube8").click(function(){
-        //   $("#franks").css({display:"block"});
-        // })
-
-        // $("#cube3").click(function(){
-        //   $("#vanpels").css({display:"block"});
         })
 
-
-        $("#cube4").click(function(){
-          $("#peter").css({display:"block"});
-        })
-
-        $("#cube2").click(function(){
-          $("#cube3modal").css({display:"block"});
-        })
-
+      //close the modal when the close sign is clicked on
         $(".close").click(function(){
             $(this).parents(".modal").css({display:"none"});
             overlay.remove();
-            //console.log("close");
         })
 
 
